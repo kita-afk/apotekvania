6c3662daf589282d0b66943231b87430fb187d33
kita-afk <laptopl293@gmail.com>
2022-06-30 14:43:34 +0700

    tesssshkjhjkt

--- application/controllers/Example.php
+++ application/controllers/Example.php
@@ -32,9 +32,6 @@ class Example extends CI_Controller
 		// $this->template->write('title', 'Dashboard', TRUE);
 		// $this->template->write('header', 'Dashboard');
 		// $this->template->write_view('content', 'tes/login', '', true);
-		$m = $this->db->query("SELECT * FROM user")->row();
-		var_dump($m);
-		die;
 		$this->load->view("tes/login");
 		// $this->template->render();
 	}
